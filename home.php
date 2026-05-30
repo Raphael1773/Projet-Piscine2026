2dcb02f4f8f903f8eb9bf7dac5b523fcb06e49cb
Popi689 <DylanGiau@gmail.com>
2026-05-30 07:53:44 +0200

    Create home.php

---
+++ home.php
@@ -0,0 +1,103 @@
+<title>Mercato Nova</title>
+
+<link rel="stylesheet"
+      href="front_end/style.css">
+<div class="navbar">
+
+    <div class="left-section">
+
+        <div class="logo">
+            LOGO
+        </div>
+
+        <div class="site-name">
+            Mercato Nova
+        </div>
+
+    </div>
+
+    <div class="search-section">
+
+        <input
+            type="text"
+            placeholder="Recherche..."
+            class="search-bar"
+        >
+
+    </div>
+
+    <div class="right-section">
+
+        <a href="panier.php" class="icon-link">
+            Panier
+        </a>
+
+        <a href="profile.php" class="icon-link">
+            Profile
+        </a>
+
+    </div>
+
+</div>
+
+<div class="menu-buttons">
+
+    <button>Produits</button>
+
+    <button>Encheres</button>
+
+    <button>Particuliers</button>
+
+</div>
+
+<div class="products-grid">
+
+    <div class="product-card">
+
+        <img
+		src="images/prismatic_evolution_booster_pack.jpg"
+		class="product-image">
+
+        <p>Prismatic evolution booster pack</p>
+
+        <p>20 €</p>
+
+        <button>
+            Ajouter au panier
+        </button>
+
+    </div>
+
+    <div class="product-card">
+
+        <img
+		src="images/snivy_plush.jpg"
+		class="product-image">
+
+        <p>Snivy plush</p>
+
+        <p>30 €</p>
+
+        <button>
+            Ajouter au panier
+        </button>
+
+    </div>
+
+    <div class="product-card">
+
+        <img
+		src="images/red_panda_plush.jpg"
+		class="product-image">
+
+        <p>Red panda plush</p>
+
+        <p>40 €</p>
+
+        <button>
+            Ajouter au panier
+        </button>
+
+    </div>
+
+</div>
